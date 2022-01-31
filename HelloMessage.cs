c1b2baa0c62384ba99cc8ace265172f9593a6034
paulb150 <paul.broekman@gmail.com>
2022-01-31 14:24:20 +0200

    Initial Commit

---
+++ HelloMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello.World
+{
+    public class HelloMessage
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public HelloMessage(string firstName, string lastName)
+        {
+            this.LastName = lastName;
+            this.FirstName = firstName;
+        }
+
+        public string GetMessage(){
+            return $"Hello, {FirstName} {LastName}";
+        }
+    }
+}
